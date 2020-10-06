1b36039a9d410f7f68d6d0bce8826119abbd248e
Iliyan-Y <maximizebg@gmail.com>
2020-10-06 08:19:20 +0100

    Acceptance Criteria test refactured to work on evry day

--- spec/acceptanceCriteriaSpec.js
+++ spec/acceptanceCriteriaSpec.js
@@ -4,8 +4,9 @@ describe('Acceptance criteria', () => {
     bank.deposit(1000);
     bank.deposit(2000);
     bank.withdraw(500);
-    expected =
-      'date || credit || debit || balance\n05/10/2020 ||  || 500.00 || 2500.00\n05/10/2020 || 2000.00 ||  || 3000.00\n05/10/2020 || 1000.00 ||  || 1000.00';
+    let date = new Date().toISOString().split('T')[0];
+    let formateDate = bank._formatDate(date);
+    expected = `date || credit || debit || balance\n${formateDate} ||  || 500.00 || 2500.00\n${formateDate} || 2000.00 ||  || 3000.00\n${formateDate} || 1000.00 ||  || 1000.00`;
     expect(bank.printStatement()).toEqual(expected);
   });
 });
